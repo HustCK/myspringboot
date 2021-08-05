e8adabbf5f5b3d05c63e6c63256d03db14ac94e7
HustCK <826764541@qq.com>
2021-08-05 21:12:24 +0800

    Initialization

---
+++ src/main/java/com/example/myspring/MyspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyspringApplication.class, args);
+    }
+
+}
